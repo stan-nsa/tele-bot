e366c6ba8654d1f05cea13f09b20c3fbb8910960
stan-nsa <stan.nsa@gmail.com>
2024-07-23 01:21:55 +1000

    nsa

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import asyncio
 import logging
+#import sys
+
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+from environs import Env
+
+env = Env()
+env.read_env()
+
+async def main():
+    dp = Dispatcher()
+    bot = Bot(token=env('BOT_TOKEN'), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    await dp.start_polling(bot)
 
 
-async def main()
-
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)#, stream=sys.stdout)
+    asyncio.run(main())
