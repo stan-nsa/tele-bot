2faaf2b6c59d46ad0de2ca91a104527aa067093a
stan-nsa <stan.nsa@gmail.com>
2024-07-24 01:12:49 +1000

    nsa

---
+++ handlers/commands/commands_menu.py
@@ -0,0 +1,6 @@
+from aiogram.types import BotCommand
+
+
+commands_menu = [BotCommand(command='start', description='Запуск бота'),
+                 BotCommand(command='help', description='Помощь'),
+                 BotCommand(command='admin', description='Админское меню')]
--- main.py
+++ main.py
@@ -6,8 +6,10 @@ from environs import Env
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
+from aiogram.types import BotCommandScopeDefault
 
 from handlers import router as handlers_router
+from handlers.commands.commands_menu import commands_menu
 
 env = Env()
 env.read_env()
@@ -19,8 +21,11 @@ async def main():
 
     bot = Bot(token=env('BOT_TOKEN'),
               default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-
+    # Прописываем меню команд для всех
+    await bot.set_my_commands(commands_menu, BotCommandScopeDefault())
+    # Удаляем неполученные/необработанные обновления/сообщения
     await bot.delete_webhook(drop_pending_updates=True)
+
     await dp.start_polling(bot)
 
 
