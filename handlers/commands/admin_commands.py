6f90ad4e785be7211a574594a54aa148af494dcc
stan-nsa <stan.nsa@gmail.com>
2024-08-06 01:04:20 +1000

    nsa

--- handlers/commands/admin_commands.py
+++ handlers/commands/admin_commands.py
@@ -8,4 +8,4 @@ router = Router(name=__name__)
 @router.message(Command('admin', ignore_case=True))
 async def handler_command_admin(message: types.Message):
     await message.answer(
-        text=f"А ты точно Админ? 😏 !")
+        text=f"А ты точно Админ? 😏")
