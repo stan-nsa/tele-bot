755930029b0a34275d1af9ae9baa265f454f1409
stan-nsa <stan.nsa@gmail.com>
2024-10-04 22:17:04 +1000

    nsa

--- handlers/fsm/__init__.py
+++ handlers/fsm/__init__.py
@@ -10,15 +10,15 @@ router = Router()
 router.include_router(sku_roter)
 
 # Роутер только для лички (фильтры уже прописаны в /handlers/__init__.py)
-router.message.filter(
-    and_f(
-        F.chat.type == 'private',
-        or_f(
-            lambda demo: config.demo,
-            F.from_user.id.in_(config.bot.admins),
-        )
-    )
-)
+# router.message.filter(
+#     and_f(
+#         F.chat.type == 'private',
+#         or_f(
+#             lambda demo: config.demo,
+#             F.from_user.id.in_(config.bot.admins),
+#         )
+#     )
+# )
 
 # router.message.filter(F.chat.type == 'private')
 # router.callback_query.filter(F.chat.type == 'private')
